c3c541ec9c944b04e3e860a51f3d6d4a11a2ec83
Yann <sltcyann@icloud.com>
2026-08-15 03:57:18 +0200

    depuis 0 allez

--- b0urse/b0urseApp.swift
+++ b0urse/b0urseApp.swift
@@ -1,30 +1,10 @@
-//
-//  b0urseApp.swift
-//  b0urse
-//
-//  Created by Yann on 15/08/2026.
-//
-
-import AppKit
 import SwiftUI
 
 @main
 struct b0urseApp: App {
-    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
-
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .frame(minWidth: 520, minHeight: 640)
         }
-        .defaultSize(width: 580, height: 720)
-        .windowResizability(.contentMinSize)
-    }
-}
-
-@MainActor
-private final class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        true
     }
 }
